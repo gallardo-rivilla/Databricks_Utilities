e738baa8c4d9c2ec0893d5d134efc3546338b405
gallardo-rivilla <gallardo-rivilla>
2024-12-12 15:44:46 +0000

    Apache Iceberg

---
+++ 7. Apache Iceberg.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # 🧊  Apache Iceberg 👉Un formato para gobernarlos a todos {🎆LakeHouse🎆} [2024/25]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ![](https://gallardorivilla.es/wp-content/uploads/2024/12/APACHE-ICEBERG.png)
+
+# COMMAND ----------
+
+
